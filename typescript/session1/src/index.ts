48c68d77e76adfb8c26f6f5131feecd77980ee84
Priyadharshini R <priyadharshinirameshkumar19@gmail.com>
2026-06-18 17:14:11 +0530

    typescript/session1: complete task 1.2 and strict mode examples

--- typescript/session1/src/index.ts
+++ typescript/session1/src/index.ts
@@ -1,1 +1,5 @@
 console.log("TypeScript is running");
+
+const age: number = 30;
+// We use tsc --noEmit when we only want to check for TypeScript errors.
+// It validates the code without generating JavaScript files in the dist folder.
